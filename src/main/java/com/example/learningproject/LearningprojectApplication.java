028957f320cf0e0cd817bdadf41f7d32d0eef313
Sai kumar kolisetty <saikumar.kolisetty@gmail.com>
2022-09-12 08:18:01 +0530

    Added code to run learning project

--- src/main/java/com/example/learningproject/LearningprojectApplication.java
+++ src/main/java/com/example/learningproject/LearningprojectApplication.java
@@ -1,13 +1,22 @@
 package com.example.learningproject;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningprojectApplication {
 
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(LearningprojectApplication.class, args);
 	}
 
-}
+}}
